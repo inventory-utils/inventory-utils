898fcab7442d6633042e019969d2784ba7ad6486
Grzegorz Wierzowiecki <gwpublic@wp.pl>
2016-05-21 00:59:56 +0200

    where+= first datastructures, generation of new items in forest

--- src/inventory-where-backend-main.cpp
+++ src/inventory-where-backend-main.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <set>
 #include <string>
+#include <vector>
 
 // TODO: move datstructures and some of includes to src/*-datastructures.{h,c}pp files.
 
@@ -11,25 +12,42 @@ using std::cout;
 using std::map;
 using std::set;
 using std::string;
+using std::vector;
 
-// uids / Unique IDs of items:
-// * uid<0 means it's temporary, internal uid, not allowed to be used by user.
-// * uid==0 is root vertex of tree of items
-// * uid>0 is Unique ID visible to user, and can be used by user.
-using uid_t = int64_t;
+// iuids / Item Unique IDs of items:
+// * iuid<0 means it's temporary, internal uid, not allowed to be used by user.
+// * iuid==0 is root vertex of tree of items
+// * iuid>0 is Unique ID visible to user, and can be used by user.
+using iuid_t = int64_t;
 
 class item_t {
+	private:
+		iuid_t _my_iuid;
 	public:
-		set<uid_t> children;
+		set<iuid_t> children;
 		// TODO: '$key=val$' to be implemented
 		// map<string,
+
+		item_t(iuid_t uid): _my_iuid(uid) {}
+		iuid_t iuid() { return _my_iuid; }
 };
 
-map<uid_t, item_t> 
+class forest : public vector<item_t> {
+	public:
+		item_t& add_new_item(){
+			iuid_t new_iuid = -size();
+			emplace_back(new_iuid);
+			return back();
+		}
+};
 
 int main(){
-	int64_t i;
-	cin >> i;
-	cout << "# " << i << " #" << std::endl;
+	forest F;
+	auto i1 = F.add_new_item();
+	auto i2 = F.add_new_item();
+	auto i3 = F.add_new_item();
+	cout << "new iuid: " << i1.iuid() << std::endl;
+	cout << "new iuid: " << i2.iuid() << std::endl;
+	cout << "new iuid: " << i3.iuid() << std::endl;
 	return 0;
 }
